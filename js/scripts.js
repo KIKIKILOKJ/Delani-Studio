098f54c40db86c562ee29e04942dabd017d5572e
peter max <petermax2004@gmail.com>
2019-05-04 17:26:07 +0300

    Add the jquery effects and alert to the webpage

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,9 @@ $(document).ready(function(){
       $("#left").toggle();
     });
   });
+
+  $(document).ready(function(){
+    $(".lolo").click(function(){
+      alert("Hello!! We have received your message.Thank you for reaching out to us");
+    })
+  })
