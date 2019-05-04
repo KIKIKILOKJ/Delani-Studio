3e01aca9dcb19fa57313f4de117f138d5cf6b8b3
peter max <petermax2004@gmail.com>
2019-05-04 16:19:55 +0300

    Add the codes to the webpage so as to design the webpage accordingly

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $("#hid1").click(function(){
+    $("#up").toggle();
+   });
+  });
+
+  $(document).ready(function(){
+    $("#hid2").click(function(){
+      $("#down").toggle();
+    });
+  });
+
+  $("document").ready(function(){
+    $("#hid3").click(function(){
+      $("#left").toggle();
+    });
+  });
